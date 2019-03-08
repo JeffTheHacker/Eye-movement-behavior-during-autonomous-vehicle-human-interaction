96101a97e4eb52b78f30af55ff07b7967cf4a977
Jeff He <jeff2022_eecs@berkeley.edu>
2019-03-08 15:46:59 -0800

    added processing code

---
+++ video_detection.py
@@ -0,0 +1,35 @@
+from object_detection_applied import processImages
+from video_to_frames import convert
+import pickle
+
+imgArr = []
+dictArr = []
+finalTable = []
+convert("/Users/jeffhe/Desktop/commitments/urap/week 1/world.mp4","/Users/jeffhe/Desktop/commitments/urap/week 1/frames/")
+imgArr, dictArr = processImages("/Users/jeffhe/Desktop/commitments/urap/week 1/frames",357) #miny,minx,maxy,maxx
+
+height,width,channel = imgArr[0].shape
+
+i = 0
+while (i < len(dictArr)):
+    dictionary = dictArr[i]
+    boxes = dictionary['detection_boxes']
+    classes = dictionary['detection_classes']
+    scores = dictionary['detection_scores']
+    j = 0
+    print(boxes)
+    print(classes)
+    while (j < len(boxes)):
+        if (not (boxes[j][0] == 0 and boxes[j][1] == 0 and boxes[j][2] == 0 and boxes[j][3] == 0)) and classes[j] == 1:
+            miny = int(boxes[j][0] * height)
+            minx = int(boxes[j][1] * width)
+            maxy = int(boxes[j][2] * height)
+            maxx = int(boxes[j][3] * width)
+            row = [i,miny,minx,maxy,maxx,scores[j]]
+            finalTable.append(row)
+        j += 1
+    i += 1
+
+print(finalTable)
+pickle.dump(finalTable,open("finalTable.pickle",'wb'))
+
